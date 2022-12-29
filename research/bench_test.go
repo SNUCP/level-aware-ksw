5ce315bd2747ac8fd1a5c573c0c5e81df44bea16
issac-seo <issac_seo@kaist.ac.kr>
2022-12-29 11:26:31 +0900

    modified benchmark

--- research/bench_test.go
+++ research/bench_test.go
@@ -11,8 +11,13 @@ import (
 )
 
 var qpCount = 40
-var pCounts = []int{4}                     // k
-var levels = []int{32 - 1, 28 - 1, 24 - 1} // Change this to: 35, 31, 27, 23, 19
+var pCount = 1
+var levels = []int{
+	39 - 1, 38 - 1, 37 - 1, 36 - 1,
+	32 - 1, 28 - 1, 24 - 1, 20 - 1,
+	16 - 1, 12 - 1, 8 - 1, 4 - 1,
+}
+
 var paramsLiteral = ckks.ParametersLiteral{
 	LogN:     16,
 	LogSlots: 15,
@@ -43,39 +48,34 @@ func BenchmarkKeySwitch(b *testing.B) {
 	defaultP := make([]uint64, len(paramsLiteral.P))
 	copy(defaultP, paramsLiteral.P)
 
-	for _, pCount := range pCounts {
-		qCount := qpCount - pCount
-		paramsLiteral.Q = paramsLiteral.Q[:qCount]
-		paramsLiteral.P = paramsLiteral.P[:pCount]
+	qCount := qpCount - pCount
+	paramsLiteral.Q = paramsLiteral.Q[:qCount]
+	paramsLiteral.P = paramsLiteral.P[:pCount]
 
-		params, _ := ckks.NewParametersFromLiteral(paramsLiteral)
-		prng, _ := utils.NewKeyedPRNG([]byte{'b', 'y', 't', 'e'})
+	params, _ := ckks.NewParametersFromLiteral(paramsLiteral)
+	prng, _ := utils.NewKeyedPRNG([]byte{'b', 'y', 't', 'e'})
 
-		kgen := ckks.NewKeyGenerator(params)
-		sk := kgen.GenSecretKey()
-		skOut := kgen.GenSecretKey()
-		swk := kgen.GenSwitchingKey(sk, skOut)
-		evaluator := ckks.NewEvaluator(params, rlwe.EvaluationKey{})
-		ksw := evaluator.GetKeySwitcher()
+	kgen := ckks.NewKeyGenerator(params)
+	sk := kgen.GenSecretKey()
+	skOut := kgen.GenSecretKey()
+	swk := kgen.GenSwitchingKey(sk, skOut)
+	evaluator := ckks.NewEvaluator(params, rlwe.EvaluationKey{})
+	ksw := evaluator.GetKeySwitcher()
 
-		for _, level := range levels {
-			for _, levelSP := range []int{4 - 1, 8 - 1, 12 - 1} {
-				if level+levelSP+2 > qpCount {
-					continue
-				}
-				ksw.LevelSP[level] = levelSP
-				testName := fmt.Sprintf("L-%v/l-%v/r-%v/SP-%v", qCount, level+1, pCount, levelSP+1)
+	for _, level := range levels {
 
-				ctIn := ckks.NewCiphertextRandom(prng, params, 1, level, params.DefaultScale())
-				ctOut := ckks.NewCiphertextRandom(prng, params, 1, level, params.DefaultScale())
-
-				b.Run(testName, func(b *testing.B) {
-					for i := 0; i < b.N; i++ {
-						evaluator.SwitchKeys(ctIn, swk, ctOut)
-					}
-				})
-			}
+		if level > params.MaxLevel() {
+			continue
 		}
-	}
 
+		testName := fmt.Sprintf("L-%v/l-%v/SP-%v", qCount, level+1, ksw.LevelSP[level]+1)
+		ctIn := ckks.NewCiphertextRandom(prng, params, 1, level, params.DefaultScale())
+		ctOut := ckks.NewCiphertextRandom(prng, params, 1, level, params.DefaultScale())
+
+		b.Run(testName, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				evaluator.SwitchKeys(ctIn, swk, ctOut)
+			}
+		})
+	}
 }
